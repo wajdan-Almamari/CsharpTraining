647a4a9bf306bb812dd86c190cfe2b6c48fbd459
wajdan <wejdan95.almamari@gmail.com>
2026-05-12 11:58:01 +0400

    Task2 : ATM Services

--- Program.cs
+++ Program.cs
@@ -38,10 +38,43 @@ namespace BankingServices
             int choice =int.Parse(Console.ReadLine());
 
             //switch option 
-
-            switch(choice){
+            //Task2
+            switch (choice){
                 case 1: 
-                    Console.WriteLine("1: ATM Services ");
+                    Console.WriteLine("=== ATM SERVICES === ");
+                    Console.WriteLine("1) Bank Info");
+                    Console.WriteLine("2) Branch Info");
+                    Console.WriteLine("3) Opening Hours");
+                    Console.WriteLine("0) Back To Main Menu");
+                    Console.Write("Select: ");
+                    int atmChoice =int.Parse(Console.ReadLine());
+                    switch (atmChoice)
+                    { 
+                       
+                        case 1:// Bank Information
+                            Console.WriteLine("National Bank Of Oman");
+                            Console.WriteLine("Your Trusted Banking Partner");
+                            Console.WriteLine(" founding: 2026");
+                            break;
+                        case 2:// Branch Information
+                            Console.WriteLine("Muscat branch name ");
+                            Console.WriteLine(" city: Muscat");
+                            Console.WriteLine(" address");
+                            break;
+                        case 3:// Opening Hours
+                            Console.WriteLine("Weekdays: 8 AM - 3 PM");
+         
+                            Console.WriteLine("Weekend: Closed");
+                            break;
+
+                        case 0:// Back To Main Menu
+                            Console.WriteLine("Returning to Main Menu...");
+                            break;
+                        default:
+                            Console.WriteLine("'Invalid selection. Please try again.");
+                            break;
+                    }
+
                     break;
                 case 2:
                     Console.WriteLine("2: Account Management ");
@@ -61,7 +94,7 @@ namespace BankingServices
                 case 7:
                     Console.WriteLine("7: Reports & Admin ");
                     break;
-                defult: 
+                default: 
                     Console.WriteLine("Invalid Number ");
                     break;
             }
