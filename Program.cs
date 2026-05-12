8808eab9ec80e59c24d626e6488f46985a9841c9
wajdan <wejdan95.almamari@gmail.com>
2026-05-12 09:13:46 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
