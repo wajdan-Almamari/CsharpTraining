a8ec332399551792e04777bbdb7be7208376314b
wajdan <wejdan95.almamari@gmail.com>
2026-05-12 10:34:11 +0400

    Task 1: setup screen

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace CsharpTraining
+﻿namespace BankingServices
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //declare variables
+
+
+
+
+            //system options
+            //dispay main Menu
+
             Console.WriteLine("NATIONAL BANK OF OMAN  —  Unified Banking System");
+            Console.WriteLine("Task 1  ->  System Setup (populate all shared variables first)");
+          
+            Console.WriteLine("1: ATM Services ");
+            Console.WriteLine("2: Account Management ");
+            Console.WriteLine("3: Loan Services  ");
+            Console.WriteLine("4: Currency Exchange");
+            Console.WriteLine("5: Credit Card Portal ");
+            Console.WriteLine("6: Branch Services ");
+            Console.WriteLine("7: Reports & Admin ");
+            Console.WriteLine("Select Option");
+            int choice =int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
         }
     }
 }
