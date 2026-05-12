4697b9ac3152345fc47db33ad3ea1f9f609b50f0
wajdan <wejdan95.almamari@gmail.com>
2026-05-12 09:55:27 +0400

    DISPLAY NATIONAL BANK OF OMAN

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Wajdan!");
+            Console.WriteLine("NATIONAL BANK OF OMAN  —  Unified Banking System");
         }
     }
 }
