353f0d85e2f081ac583caf6b4d012f9860a75035
wajdan-Almamari <wejdan95.almamari@gmail.com>
2026-05-15 10:59:47 +0400

    Created CMS Project

---
+++ ClinicManagement-System-CMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicManagement_System_CMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
