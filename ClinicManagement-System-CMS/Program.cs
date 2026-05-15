2ac1160584eefd8a84a7efc20d26cac93fdc9e0e
wajdan-Almamari <wejdan95.almamari@gmail.com>
2026-05-15 11:02:30 +0400

    Adedd REGION 1: System Storage

--- ClinicManagement-System-CMS/Program.cs
+++ ClinicManagement-System-CMS/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // ── REGION 1: System Storage ───────────────────────────────────── 
+            // Capacity constants 
+            const int MAX_PATIENTS = 3;
+            const int MAX_DOCTORS = 2;
+            const int MAX_APPOINTMENTS = 3;
+
+            // Patient slots 
+            string p1Name = ""; int p1Age = 0; string p1Phone = ""; bool p1Active = false;
+            string p2Name = ""; int p2Age = 0; string p2Phone = ""; bool p2Active = false;
+            string p3Name = ""; int p3Age = 0; string p3Phone = ""; bool p3Active = false;
+            int patientCount = 0;
+
+
+            // Doctor slots 
+            string d1Name = ""; string d1Spec = ""; double d1Fee = 0; bool d1Active = false;
+            string d2Name = ""; string d2Spec = ""; double d2Fee = 0; bool d2Active = false;
+            int doctorCount = 0;
+
+            // Appointment slots 
+            string a1Patient = ""; string a1Doctor = ""; string a1Date = "";
+            string a1Status = ""; bool a1Active = false;
+            string a2Patient = ""; string a2Doctor = ""; string a2Date = "";
+            string a2Status = ""; bool a2Active = false;
+            string a3Patient = ""; string a3Doctor = ""; string a3Date = "";
+            string a3Status = ""; bool a3Active = false; int appointmentCount = 0;
+
         }
     }
 }
